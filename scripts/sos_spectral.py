d7a5a0de1d18a944c9c5d58788fb413917bdb38a
WilsonGregory <wilson.g.gregory@gmail.com>
2024-04-15 12:43:48 -0400

    update sos script, add more types of sparse vector initializations

--- scripts/sos_spectral.py
+++ scripts/sos_spectral.py
@@ -10,6 +10,10 @@ import optax
 import tensorpolynomials.data as tpoly_data
 import tensorpolynomials.ml as ml
 
+RAND_COV = 'random_covariance'
+DIAG_COV = 'diagonal_covariance'
+UNIT_COV = 'unit_covariance'
+
 def sos_method(S):
     """
     The sum-of-squares method from: https://arxiv.org/pdf/1512.02337.pdf
@@ -24,6 +28,24 @@ def sos_method(S):
 
     _, eigvecs = jnp.linalg.eigh(A) # ascending order
     assert eigvecs.shape == (d,d)
+    u = eigvecs[...,-1] # the eigenvector corresponding to the top eigenvalue
+    return S @ u
+
+def sos_methodII(S):
+    """
+    The sum-of-squares method from the newer: https://arxiv.org/pdf/2105.15081.pdf
+    We match the notation of that paper with (N,n) rather than (n,d)
+    args:
+        S (jnp.array): an (N,n) array, where N is the ambient dimension and n is num of vectors
+    """
+    N,n = S.shape
+
+    vmap_inner = jax.vmap(lambda ai: (jnp.linalg.norm(ai)**2 - ((n-1)/N))*jnp.tensordot(ai,ai,axes=0))
+    A = jnp.sum(vmap_inner(S),axis=0) - 3*jnp.eye(n)
+    assert A.shape == (n,n)
+
+    _, eigvecs = jnp.linalg.eigh(A) # ascending order
+    assert eigvecs.shape == (n,n)
     u = eigvecs[...,-1] # the eigenvector corresponding to the top eigenvalue
     return S @ u
 
@@ -150,68 +172,85 @@ key = random.PRNGKey(time.time_ns())
 key, subkey = random.split(key)
 
 # define data params
-n = 10
-d = 3
+n = 100
+d = 5
 eps = 0.3
-train_size = 5000
+train_size = 2000
 val_size = 500
 test_size = 500
 
-# Random covariance matrix, force it to be pos. def.
-# M = random.normal(subkey, shape=(n,n))
-# cov = M.T @ M + (1e-5)*jnp.eye(n)
-
-# Random diagonal covariance matrix
-cov = jnp.diag(random.uniform(subkey, shape=(n,)))
-
-# No covariance, use 1/n * Id
-# cov = None
-
 # define training params
+width = 128
+depth = 2
 batch_size = 100
 learning_rate = 3e-4
-trials = 3
+trials = 5
 verbose = 0
 
 key, subkey1, subkey2 = random.split(key, 3)
 models = [
-    ('sparseDiagonal', SparseVectorHunterDiagonal(n, 128, 2, subkey1)), # n inputs, n+1 outputs
-    ('sparse', SparseVectorHunter(n, 128, 2, subkey2)), # n**2 inputs, n+1+(n(n-1)/2) outputs
+    ('sparseDiagonal', SparseVectorHunterDiagonal(n, width, depth, subkey1)), # n inputs, n+1 outputs
+    ('sparse', SparseVectorHunter(n, width, depth, subkey2)), # n**2 inputs, n+1+(n(n-1)/2) outputs
 ]
 for model_name, model in models:
     print(f'{model_name}: {sum([x.size for x in jax.tree_util.tree_leaves(model)]):,} params')
     
-results = np.zeros((trials,len(models)+1,2))
+samplings = [tpoly_data.V0_NORMAL,tpoly_data.V0_BERN_GAUS,tpoly_data.V0_BERN_RAD,tpoly_data.V0_KSPARSE]
+covariances = [RAND_COV, DIAG_COV, UNIT_COV]
+
+results = np.zeros((trials,len(samplings),len(covariances),len(models)+2,2))
 
 for t in range(trials):
-    key, subkey1, subkey2, subkey3 = random.split(key, 4)
+    for i, v0_sampling in enumerate(samplings):
+        for j, cov_type in enumerate(covariances):
+            key, subkey1, subkey2, subkey3, subkey4 = random.split(key, 5)
 
-    # (batch,n), (batch,n,d)
-    train_v0, train_W = tpoly_data.get_synthetic_data(subkey1, n, d, eps, train_size, cov) 
-    val_v0, val_W = tpoly_data.get_synthetic_data(subkey2, n, d, eps, val_size, cov)
-    test_v0, test_W = tpoly_data.get_synthetic_data(subkey3, n, d, eps, test_size, cov)
+            if cov_type == RAND_COV:
+                # Random covariance matrix, force it to be pos. def.
+                M = random.normal(subkey4, shape=(n,n))
+                cov = M.T @ M + (1e-5)*jnp.eye(n)
+            elif cov_type == DIAG_COV:
+                # Random diagonal covariance matrix
+                cov = jnp.diag(random.uniform(subkey, shape=(n,)))
+            elif cov_type == UNIT_COV:
+                # No covariance, use 1/n * Id
+                cov = None
+            else:
+                raise Exception(f'No covariance {cov}')
 
-    results[t,0,0] = 1 - map_and_loss(sos_method, train_W, train_v0)
-    results[t,0,1] = 1 - map_and_loss(sos_method, test_W, test_v0)
+            # (batch,n), (batch,n,d)
+            train_v0, train_W = tpoly_data.get_synthetic_data(subkey1, n, d, eps, train_size, v0_sampling, cov) 
+            val_v0, val_W = tpoly_data.get_synthetic_data(subkey2, n, d, eps, val_size, v0_sampling, cov)
+            test_v0, test_W = tpoly_data.get_synthetic_data(subkey3, n, d, eps, test_size, v0_sampling, cov)
 
-    for i, (model_name, model) in enumerate(models):
-        print(f'trial: {t}, model: {model_name}')
-        key, subkey = random.split(key)
-        trained_model, _, _ = ml.train(
-            model, 
-            map_and_loss, 
-            train_W, 
-            train_v0, 
-            subkey, 
-            ml.ValLoss(patience=20, verbose=verbose),
-            optax.adam(optax.exponential_decay(learning_rate, int(train_size/batch_size), 0.999)), 
-            batch_size, 
-            val_W, 
-            val_v0,
-        )
+            results[t,i,j,0,0] = 1 - map_and_loss(sos_method, train_W, train_v0)
+            results[t,i,j,0,1] = 1 - map_and_loss(sos_method, test_W, test_v0)
+            print(f'{t},{v0_sampling},{cov_type},sos: {results[t,i,j,0,1]}')
 
-        results[t,i+1,0] = 1 - map_and_loss(trained_model, train_W, train_v0)
-        results[t,i+1,1] = 1 - map_and_loss(trained_model, test_W, test_v0)
+            results[t,i,j,1,0] = 1 - map_and_loss(sos_methodII, train_W, train_v0)
+            results[t,i,j,1,1] = 1 - map_and_loss(sos_methodII, test_W, test_v0)
+            print(f'{t},{v0_sampling},{cov_type},sosII: {results[t,i,j,1,1]}')
+
+            for k, (model_name, model) in enumerate(models):
+                key, subkey = random.split(key)
+                trained_model, _, _ = ml.train(
+                    model, 
+                    map_and_loss, 
+                    train_W, 
+                    train_v0, 
+                    subkey, 
+                    ml.ValLoss(patience=20, verbose=verbose),
+                    optax.adam(optax.exponential_decay(learning_rate, int(train_size/batch_size), 0.999)), 
+                    batch_size, 
+                    val_W, 
+                    val_v0,
+                )
+
+                results[t,i,j,k+2,0] = 1 - map_and_loss(trained_model, train_W, train_v0)
+                results[t,i,j,k+2,1] = 1 - map_and_loss(trained_model, test_W, test_v0)
+                print(f'{t},{v0_sampling},{cov_type},{model_name}: {results[t,i,j,k+2,1]}')
+            
+            jnp.save(f'../runs/sparse_vector_results_lr{learning_rate}_n{n}_d{d}_eps{eps}.npy', results)
 
 print(results)
 print(jnp.mean(results, axis=0))
--- src/tensorpolynomials/data.py
+++ src/tensorpolynomials/data.py
@@ -1,33 +1,98 @@
 from tqdm import tqdm
+import numpy as np
 
 import jax
 import jax.numpy as jnp
 import jax.random as random
 
-def get_sparse_vector(key, n, eps, batch):
+TINY = 1e-5
+
+V0_NORMAL = 'v0_normal'
+V0_BERN_GAUS = 'v0_bernoulli_gaussian'
+V0_BERN_RAD = 'v0_bernoulli_rademacher'
+V0_KSPARSE = 'v0_ksparse'
+
+def get_sparse_vector(key, n, eps, batch, v0_sampling=V0_NORMAL):
     """
-    This function uses the accept/reject method to find an L4 sparse vector. First we generate an
-    L2 unit vector in R^n. Then we accept it if norm(v)_4^4 >= 1/eps*n
+    Generate batch sparse vectors in R^n according to different strategies.
+    V0_NORMAL: generate a unit-length gaussian vector, accept if norm(v)_4^4 >= 1/eps*n else reject
+    V0_BERN_GAUS: with prob eps, v0_i is ~N(0,1/n*eps), else 0
+    V0_BERN_RAD: with prob eps/2, v0_i is 1/sqrt(n*eps), prob eps/2 v0_i is -1/sqrt(n*eps), else 0
+    V0_KSPARSE: select eps*n entries to be nonzero, sample those from a gaussian
+    No matter the sampling, we always set the vectors to unit length after generating.
     args:
         key (rand key):
         n (int): vector dimension
         eps (float): sparsity parameter, can think of this as the fraction of components that are
             nonzero, but its actually an L4 relaxation of that concept.
         batch (int): the number of sparse vectors to find
+        v0_sampling (string): one of V0_NORMAL, V0_BERN_GAUS, or V0_BERN_RAD
     """
-    print(f'generating {batch} sparse vectors...')
-    vecs = []
-    with tqdm(total=batch) as pbar:
-        while len(vecs) < batch:
+    if v0_sampling == V0_NORMAL:
+        print(f'generating {batch} sparse vectors...')
+        vecs = []
+        with tqdm(total=batch) as pbar:
+            while len(vecs) < batch:
+                key, subkey = random.split(key)
+                vec = random.normal(subkey, shape=(n,))
+                vec = vec/jnp.linalg.norm(vec)
+
+                if (jnp.sum(vec**4) >= (1/(eps*n))):
+                    vecs.append(vec)
+                    pbar.update(1)
+
+        sparse_vecs = jnp.stack(vecs)
+
+    elif v0_sampling == V0_BERN_GAUS:
+        sparse_vecs = jnp.zeros((0,n))
+
+        while len(sparse_vecs) < batch:
+            key, subkey1, subkey2 = random.split(key, 3)
+            entries = random.multivariate_normal(
+                subkey1, 
+                jnp.zeros(n), 
+                (1/(n*eps))*jnp.eye(n), 
+                shape=(batch,),
+            )
+            bernoulli = random.uniform(subkey2, shape=(batch,n))
+            vecs = jnp.where(bernoulli < eps, entries, jnp.zeros(entries.shape))
+            norms = jnp.linalg.norm(vecs, axis=1) # (batch,)
+
+            vecs = vecs[norms > TINY]
+
+            sparse_vecs = jnp.concatenate([sparse_vecs, vecs / norms[norms > TINY,None]])
+
+        sparse_vecs = sparse_vecs[:batch]
+
+    elif v0_sampling == V0_BERN_RAD:
+        sparse_vecs = jnp.zeros((0,n))
+
+        while len(sparse_vecs) < batch:
             key, subkey = random.split(key)
-            vec = random.normal(subkey, shape=(n,))
-            vec = vec/jnp.linalg.norm(vec)
+            bernoulli = random.uniform(subkey, shape=(batch,n))
+            entries = jnp.ones((batch,n))/jnp.sqrt(n*eps)
+            signed_entries = jnp.where(bernoulli < 0.5, entries, -1*entries)
+            vecs = jnp.where(bernoulli < eps, signed_entries, jnp.zeros(signed_entries.shape))
+            norms = jnp.linalg.norm(vecs, axis=1) # (batch,)
 
-            if (jnp.sum(vec**4) >= (1/(eps*n))):
-                vecs.append(vec)
-                pbar.update(1)
+            vecs = vecs[norms > 10*TINY]
+            sparse_vecs = jnp.concatenate([sparse_vecs, vecs / norms[norms > 10*TINY,None]])
 
-    return jnp.stack(vecs)
+        sparse_vecs = sparse_vecs[:batch]
+
+    elif v0_sampling == V0_KSPARSE:
+        key, subkey1, subkey2 = random.split(key, 3)
+        num_nonzero = np.floor(eps*n).astype(int) # exact number of nonzero entries per vector
+        
+        random.normal(subkey1, shape=((batch,num_nonzero)))
+        entries = jnp.concatenate(
+            [random.normal(subkey1, shape=(batch,num_nonzero)), jnp.zeros((batch,n - num_nonzero))],
+            axis=1,
+        )
+        shuffled_entries = random.permutation(subkey2, entries, axis=1, independent=True)
+        sparse_vecs = shuffled_entries / jnp.linalg.norm(shuffled_entries, axis=1, keepdims=True)
+
+    return sparse_vecs
         
 def get_gaussian_vectors(key, n, num_vecs, batch, cov):
     V = random.multivariate_normal(key, jnp.zeros(n), cov, shape=(batch,num_vecs)).transpose((0,2,1))
@@ -49,10 +114,10 @@ def get_orthogonal_basis(key, V):
     assert Q.shape == (batch,n,d)
     return Q
 
-def get_synthetic_data(key, n, d, eps, batch, cov=None):
+def get_synthetic_data(key, n, d, eps, batch, v0_sampling=V0_NORMAL, cov=None):
     cov = (1/n)*jnp.eye(n) if cov is None else cov
     key, subkey1, subkey2, subkey3 = random.split(key, 4)
-    v0 = get_sparse_vector(subkey1, n, eps, batch) # (batch,n)
+    v0 = get_sparse_vector(subkey1, n, eps, batch, v0_sampling) # (batch,n)
 
     V_noise = get_gaussian_vectors(subkey2, n, d-1, batch, cov) # (batch,n,d-1)
 
